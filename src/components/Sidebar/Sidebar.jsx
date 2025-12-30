a78872e95632bb674931d08c13265fece970992d
Onwukwennamdi <nnamdionwukwe@gmail.com>
2025-12-30 03:57:14 +0100

    wdgfvwdfg

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -71,8 +71,8 @@ const Sidebar = () => {
         <div className={styles.topAuthSection}>
           {user ? (
             <div className={styles.userInfoTop}>
-              <span className={styles.userNameTop}>👤 {user.name}</span>
-              <span className={styles.badgeTop}>{user.role}</span>
+              {/* <span className={styles.userNameTop}>👤 {user.name}</span> */}
+              {/* <span className={styles.badgeTop}>{user.role}</span> */}
               <button
                 onClick={handleLogoutClick}
                 className={styles.logoutBtnTop}
@@ -141,8 +141,8 @@ const Sidebar = () => {
           {user ? (
             <>
               <div className={styles.userInfo}>
-                {/* <span className={styles.userName}>{user.name}</span> */}
-                {/* <span className={styles.badge}>{user.role}</span> */}
+                <span className={styles.userName}>👤 {user.name}</span>
+                <span className={styles.badge}>{user.role}</span>
               </div>
               <button onClick={handleLogoutClick} className={styles.logoutBtn}>
                 Logout
